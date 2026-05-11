505f0fa32f07350d9d70b1b847334f2a84c2d3d2
katukojwala Sreemaye <Sirisreemaye@gmail.com>
2026-05-11 14:10:09 +0530

    Update API URL for employee creation

--- frontend/vite-project/src/components/CreateEmp.jsx
+++ frontend/vite-project/src/components/CreateEmp.jsx
@@ -22,7 +22,7 @@ function CreateEmp() {
     try {
       setLoading(true);
       //make HTTP POST req
-      let res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/emp-api/employees`, {
+      let res = await fetch(`https://week6-2-1.onrender.com/emp-api/employees`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newEmpObj),
